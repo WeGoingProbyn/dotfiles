a9c35fc72723eb680c1c46b352bda489a4c5e40c
William Probyn <williamprobyn123@gmail.com>
2025-04-09 23:06:16 +0100

    fix bugs in ppd-check.sh and systemd battery-profile.service

--- scripts/ppd-check.sh
+++ scripts/ppd-check.sh
@@ -10,29 +10,29 @@ send_notif() {
 }
 
 while true; do
-  status=$(cat /sys/class/power_supply/BAT0/status)
-  capacity=$(cat /sys/class/power_supply/BAT0/capacity)
-  profile=$(powerprofilectl get)
+  status=$(cat /sys/class/power_supply/BAT1/status)
+  capacity=$(cat /sys/class/power_supply/BAT1/capacity)
+  profile=$(powerprofilesctl get)
 
   if [[ "$status" == "Charging" ]]; then
     if [[ "$profile" != "performance" ]]; then
-      powerprofilectl set performance
+      powerprofilesctl set performance
       send_notif "Charging: PPD set to performance"
     fi
   else
     if [[ "$capacity" -le $LOW_THRESHOLD ]]; then
       if [[ "$profile" != "power-saver" ]]; then
-        powerprofilectl set power-saver
+        powerprofilesctl set power-saver
         send_notif "Low battery: PPD set to power-saver"
       fi
     elif [[ "$capacity" -gt $HIGH_THRESHOLD ]]; then
       if [[ "$profile" != "performance" ]]; then
-        powerprofilectl set performance
+        powerprofilesctl set performance
         send_notif "High battery: PPD set to performance"
       fi
     else
       if [[ "$profile" != "balanced" ]]; then
-        powerprofilectl set balanced
+        powerprofilesctl set balanced
         send_notif "Battery: PPD set to balanced"
       fi
     fi
